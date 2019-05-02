bc4105a9986d5afe74bf96790c5a2337a6dbb9c8
Lewis Wes Senior <lewis@wessenior.co.uk>
2019-05-02 17:51:22 +0100

    Removed duplicate scripts

--- AD-ExportUsernames.ps1
+++
@@ -1,6 +0,0 @@
-﻿$OUPath = "OU=***REMOVED***,OU=Users,OU=***REMOVED***,DC=***REMOVED***,DC=local"
-$ExportPath = "C:\Users\***REMOVED***\desktop\***REMOVED***"
-Import-Module ActiveDirectory
-Get-ADUser -Filter * -SearchBase $OUPath | Select-Object -Property SamAccountName | Export-Csv $ExportPath
-
-
